b9d299e8621117fe713b3a318c1fcecbd843fdb1
YASHHTANWARRR <141021420+YASHHTANWARRR@users.noreply.github.com>
2025-08-03 00:55:32 +0530

    upadate in ping.h/.c

---
+++ ping.c
@@ -0,0 +1,25 @@
+#include "ping .h"
+
+icmp *mkicmp(int8 type, int8 code,int8*data,int 16 size){
+    int16 n ;
+    icmp* p;
+    
+    if(!data|| !size){
+        retrun (icmp*)0;
+
+        n = sizeof(struct c_icmp)+size;
+        p = (icmp*)malloc($i n);
+        assert(p); // if no space can be allocated as we are out of nennory then we hault
+        zero($i p,n);//to empty it out 
+
+        p->type = type ;
+        p->code = code;
+        memory copy 
+        p->checksum = checksum(p);//checking the packets for any loss
+    }
+
+}
+
+int main (intc argc,char*argv[]){
+    return 0 ;
+}
--- ping.h
+++ ping.h
@@ -21,4 +21,27 @@ typedef unsigned long long int int64;
 #define $c (char *)
 #define $i (int)
 
+
+struct c_icmp{
+    int8 typ;
+    int8 code;
+    int16 checksum;
+    int8 data;
+}
+
+typedef struct c_icmp icmp ;
+
+icmp * mkicmp(int8,int8,int8*,int16);// type ,code ,data pointer and last is data 
+
 int main(int,char**);
+
+void copy(int8*dst,int8*src,int16 size){
+    int16 n ;
+    int8 *sptr,*dptr;
+
+    for (dptr=dst,srptr=src,n = size;n;n--){
+         *dptr++ == *sptr++ ; //intially they are equal but after that both of the pointer jump to the next position
+
+        return;
+    }
+}
